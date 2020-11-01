9823f66283b902a88b5755826f33cb6b35070c8a
Arshdeep-ceo <punjabistudios18@gmail.com>
2020-11-01 10:27:31 +0530

    Main Activity and vcs xml file Committed

--- app/src/main/java/com/example/androidrlocationapp/MapsActivity.java
+++ app/src/main/java/com/example/androidrlocationapp/MapsActivity.java
@@ -40,6 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
         Log.i("Tag","Message");
+        Log.i("Tag","Message");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
